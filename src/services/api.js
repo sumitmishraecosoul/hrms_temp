b932b8eb0f0b32ef83498244d6d9203f2ad86487
Harshit Kant <Harshit.Kant@ecosoulhome.com>
2025-10-10 15:17:03 +0530

    fixes

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL = (typeof import.meta !== 'undefined' && import.meta.env && import.meta.env.VITE_API_URL)
   ? import.meta.env.VITE_API_URL
-  : "http://localhost:5010";
+  : "https://api.thrivebrands-hrms.com/";
 
 const api = axios.create({
   baseURL,
